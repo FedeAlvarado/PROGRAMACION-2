a7374cef1438375317e274185eeac6a3d46f24be
FedeAlvarado <federico.alvarado96@gmail.com>
2023-08-09 20:58:15 -0300

    CLASE1

---
+++ HolaMundo.py
@@ -0,0 +1,1 @@
+print("Hola Mundo No se que hacer jaja")
---
+++ prueba.py
@@ -0,0 +1,12 @@
+suma = 3 + 1
+cadena_vacia=''
+x = str("hola")
+#multiple asignacion 
+a, b, c, d= 1,2,3,4
+#Pueden ser texto o numero, da igual.
+#PARA QUE SEA CONSTANTE LO PONEMOS EN MAYUSCULA
+PY=3,14
+print("Hola", "Mundo", sep='...',end='.\n')
+print("Hola Fede")
+y= x.lower()
+
