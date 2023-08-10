6820cfb046447b4e883d68e0d0040f21fcb7dbbf
FedeAlvarado <federico.alvarado96@gmail.com>
2023-08-09 21:40:09 -0300

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -10,3 +10,27 @@ print("Hola", "Mundo", sep='...',end='.\n')
 print("Hola Fede")
 y= x.lower()
 
+'''
+***DATASO DE DATOS***
+MULTI LINEA
+1 + 1 # 2 Suma
+8 - 1 # 7 Resta
+10 * 2 # 20 Multiplicación
+5 ** 2 # 25 Potencia
+pow(5, 2) # 25 Potencia
+25 ** 0,5 # 5 Raíz con potencia fraccionaria
+35 / 5 # 7.0 División (devuelve float)
+35 / 0 # Error
+34 // 5 # 6 División entera (trunca el cociente)
+35 % 6 # 5 Operador módulo (resto)
+3 * 2.0 # 6.0 Si uno de los operandos es float, el
+ resultado es float'''
+
+#IMPORTANTE
+'''
+Cortocircuitos por defecto:
+● True and False and 1 / 0 =#False
+● True and True and 1 / 0 =#Error
+● False or True or 1 / 0 =#True
+● False or False or 1 / 0 =#Error
+'''
