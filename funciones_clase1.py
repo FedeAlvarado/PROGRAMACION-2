632f279bb697974720b4d81852c053d48c4d181d
FedeAlvarado <federico.alvarado96@gmail.com>
2023-08-30 18:11:48 -0300

    clase miercoles

---
+++ funciones_clase1.py
@@ -0,0 +1,11 @@
+#FUNCIONES
+
+from suma_modulo  import suma as c
+
+la_suma_es = c(1,2,3)
+
+print()
+print(la_suma_es)
+
+#LE PONER FROM SI TENES EL ARCHIVO EN OTRA CARPETA.
+#SI ESTA DENTRO DE OTRA CARPETA MAS LE PONEMOS "." POR EJEMPLO : from "carpeta1.carpeta2" import "example"
---
+++ suma_modulo.py
@@ -0,0 +1,18 @@
+
+def suma(a: float,b:float = 4,c: float=1)-> float:
+
+    return a+b+c
+
+
+'''
+SI REQUIERE 2 VALORES Y LE PONEMOS SOLO 1 DA ERROR, SI PONEMOS UN DEFAUL, EL 4 EN ESTE CASO Y NO SE LE AGREGA NINGUN VALOR , TOMA EL 4
+'''
+def construir_lista(x:float, y:float, z:float) -> list:
+    lista=[]
+    lista.append(x)
+    lista.append(y)
+    lista.append(z)
+    return lista
+
+
+
